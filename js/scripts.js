c490cd5a42643d75087e1f4f7375aa14a4842d15
Krzysztof Ługowski <k.lugowski@yahoo.com>
2017-12-07 21:49:24 +0100

    Modify files

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,17 @@
-var numberOfBranches = prompt("Z ilu gałęzi ma się składać Twoja pół-choinka?");
-	for (var i = 1; i <= numberOfBranches; i++) {
-		var star = ("")
-		
-		for (var j = 0; j < i; j++) {
-			star += ("*")
-				
-		}
-		document.write(star);		
-		document.write("<br>");
-	}
+function drawTree(lvl){
+ for (var i = 1; i <= lvl; i++) {
+  var star = ''
+  var space = '';
+  for (var k = 0; k < lvl-i; k++) {
+   space += " "
+  }
+  for (var j = 0; j < 2*i-1; j++) {
+   star += "*"
+    
+  }
+  console.log(space+star); 
 
+ }
+}
 
-	
-	
-
-
-
-
-
-
-
-
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+drawTree(20);
